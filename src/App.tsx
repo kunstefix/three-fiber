a2ff0be66c41f73a40a4abb950d64b8f529d369b
Klemen Kunstek <klemen.kunstek@akasha.world>
2025-06-26 12:12:52 +0200

    Generated Jungfrau components

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
 import "./App.css";
 import { Canvas } from "@react-three/fiber";
-import { Model } from "./Etherlaken-Main_Building_F0";
-//import { Model } from "./Samplebuilding";
+import { Main_Building_F0 } from "./interlaken/Main_Building_F0";
+import { Main_Building_F0_roof } from "./interlaken/Main_Building_F0_roof";
+import { Main_Building_F1 } from "./interlaken/Main_Building_F1";
+import { Main_Building_F1_Roof } from "./interlaken/Main_Building_F1_Roof";
+import { Main_Building_Tower_Ball } from "./interlaken/Main_Building_Tower_Ball";
+import { Outer_Ring } from "./interlaken/Outer_Ring";
+import { Outer_Ring_Inner_Passages } from "./interlaken/Outer_Ring_Inner_Passages";
 import { OrbitControls } from "@react-three/drei";
 import { useState } from "react";
+import { Main_Building_Tower } from "./interlaken/Main_Building_Tower";
 
 function App() {
   const [toggle, setToggle] = useState(false);
@@ -15,7 +21,14 @@ function App() {
       <Canvas camera={{ position: [0, 0, 100] }} style={{ width: "100vw", height: "100vh" }}>
         <ambientLight intensity={0.5} />
         <pointLight position={[10, 10, 10]} />
-        <Model />
+        <Main_Building_F0 />
+        <Main_Building_F0_roof />
+        <Main_Building_F1 />
+        <Main_Building_F1_Roof />
+        <Main_Building_Tower />
+        <Main_Building_Tower_Ball />
+        <Outer_Ring />
+        <Outer_Ring_Inner_Passages />
         <OrbitControls />
       </Canvas>
     </div>
